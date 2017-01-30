47034b2f106ec25e5c9c0627deea9a16267d444f
Jordan Cotter <jrdn.cttr@gmail.com>
2017-01-30 16:56:24 -0600

    added unique array prototype

--- prototypes.js
+++ prototypes.js
@@ -5,3 +5,25 @@ String.prototype.formatCurrency = function () {
 	if (isNaN(this)) return this
 	return '$' + Number(this).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,')
 }
+//pass in property to filter out duplicate values of that property. don't pass anything in for primitive arrays
+Array.prototype.unique = function (property) {
+	if (property) {
+		var i = 0 
+		, uniqueObject = {}
+		, uniqueArray = []
+		;
+
+		for (let i = 0, n = this.length; i < n; i++) {
+			const item = this[i]
+			uniqueObject[item[property]] = item
+		}
+		for (let x in uniqueObject) {
+			uniqueArray[i++] = uniqueObject[x]
+		}
+		return uniqueArray
+	} else {
+		return this.filter(function (item, i) {
+			return this.indexOf(item) == i
+		}.bind(this))
+	}
+}
